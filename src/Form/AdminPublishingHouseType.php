bebece2fecafd5a1a07db53dd9a49f50d06e0f1a
Imane Aity <imaneaity@gmail.com>
2023-01-27 10:17:02 +0100

    exo maison d'édition: génération du formulaire

---
+++ src/Form/AdminPublishingHouseType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\PublishingHouse;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class AdminPublishingHouseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class,[
+                'label' => "nom de la maison d'édition" ,
+                'required'=> true 
+            ])
+            ->add('description', TextareaType::class,[
+                'label' => "Description de la maison d'édition",
+                'required' => true 
+            ])
+            ->add('country', CountryType::class,[
+                'required' => false,
+                'label' => "pays de la maison d'édition"
+            ])
+            ->add('send', SubmitType::class, [
+                'label' => "Envoyer",
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => PublishingHouse::class,
+        ]);
+    }
+}
