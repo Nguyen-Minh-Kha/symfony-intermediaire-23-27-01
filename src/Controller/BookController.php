7c0d72e18d33bab1d56b1f132b030b72e0ff64f8
Imane Aity <imaneaity@gmail.com>
2023-02-01 16:27:44 +0100

    Ajout des fixtures

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,11 +4,17 @@ namespace App\Controller;
 
 use App\Form\AdminBookType;
 use App\Repository\BookRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+/**
+ * @IsGranted("ROLE_ADMIN")
+ * 
+ */
 class BookController extends AbstractController
 {
     /**
