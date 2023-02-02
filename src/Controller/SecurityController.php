44a60af14b2b9a7480dc03b7c796b7e4ad0e92c1
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2023-02-02 12:17:24 +0100

    custom profile and profile page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileType;
 use App\Form\RegistrationType;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/inscription', name: 'app_security')]
+    #[Route('/inscription', name: 'app_security_registration')]
     public function registration(UserRepository $repository, Request $request, UserPasswordHasherInterface $hasher): Response
     {
 
@@ -64,6 +65,32 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * Profile route to customize profile
+     */
+    #IsGranted('ROLE_USER')
+    #[Route('/mon-profile', name: 'app_security_myProfile')]
+    public function myProfile(Request $request, UserRepository $userRepository): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(ProfileType::class, $user);
+
+        $form->handleRequest($request);
+
         
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $userRepository->save($user, true);
+
+            return $this->redirectToRoute('app_front_home_home');
+        }
+        return $this->render('security/myProfile.html.twig', [
+            'profileForm' => $form->createView(),
+        ]);
     }
 }
