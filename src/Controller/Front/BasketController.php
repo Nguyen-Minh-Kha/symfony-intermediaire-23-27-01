a1b7dcd086d31c76224e6b7fb4b51789c9d86f18
Imane Aity <imaneaity@gmail.com>
2023-02-03 12:12:43 +0100

    Exo panier: supprimer un livre du panier

--- src/Controller/Front/BasketController.php
+++ src/Controller/Front/BasketController.php
@@ -9,10 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+* @IsGranted("ROLE_USER")
+* */
 class BasketController extends AbstractController
 {
     /**
-     * @IsGranted("ROLE_USER")
+     * 
      * @Route("/mon-panier/{id}/ajouter", name="app_front_basket_add")
      */
     public function add(Book $book, BasketRepository $repository): Response
@@ -36,11 +39,34 @@ class BasketController extends AbstractController
 
 
     /**
-     * @IsGranted("ROLE_USER")
+     * 
      * @Route("/mon-panier", name="app_front_basket_display")
      */
     public function display( BasketRepository $repository): Response
     {
         return $this->render('front/basket/display.html.twig');
+    }
+
+
+        /**
+     * 
+     * @Route("/mon-panier/{id}/supprimer", name="app_front_basket_remove")
+     */
+    public function remove(Book $book, BasketRepository $repository): Response
+    {
+        //Récupérer le panier de l'utilisateur connécté et supprimer le livre (eg: `$basket->removeBook($book)`).
+        
+        //récupérer l'utilisateur
+        $user=$this->getUser();
+        //récuprer la panier de l'utilisateur connecté
+        $basket = $user->getBasket();
+        //supprimer le livre du panier
+        $basket->removeBook($book);
+
+        //Enregistré le panier dans la base de données.
+        $repository->add($basket , true);
+
+        //Rediriger vers la page d'affichage du panier.
+        return $this->redirectToRoute('app_front_basket_display');
     }
 }
