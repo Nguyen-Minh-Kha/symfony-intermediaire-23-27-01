6d4369975fe4bdd37702cc2e0e62c029b4c29edc
Imane Aity <imaneaity@gmail.com>
2023-02-03 14:22:06 +0100

    Exo panier: Finalisation des commandes

--- src/Controller/Front/BasketController.php
+++ src/Controller/Front/BasketController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller\Front;
 
+use App\DTO\Payment;
 use App\Entity\Book;
+use App\Entity\Order;
+use App\Form\PaymentType;
+use App\Repository\UserRepository;
+use App\Repository\OrderRepository;
 use App\Repository\BasketRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -68,5 +74,65 @@ class BasketController extends AbstractController
 
         //Rediriger vers la page d'affichage du panier.
         return $this->redirectToRoute('app_front_basket_display');
+    }
+
+
+
+    /**
+     * 
+     * @Route("/mon-panier/validation", name="app_front_basket_validate")
+     */
+    public function validate(Request $request, BasketRepository $repository, UserRepository $userrepo, OrderRepository $orderrepo): Response
+    {
+
+        //Création du paiment
+        $payment= new Payment();
+
+        //Récuperation de l'utilisateur connecté
+        $user= $this->getUser();
+        // Récupere l'addresse de livraison
+        $payment->address = $user->getDeliveryAddress();
+
+        //création du form de paiement
+        $form = $this->createForm(PaymentType::class , $payment);
+
+        // remplissage du form
+        $form->handleRequest($request);
+
+        //si le formulaire est envoyé et est valide
+        if($form->isSubmitted() && $form->isValid()){
+
+            //créer la commande
+            $order= new Order();
+
+            // attacher la commande à l'utilisateur
+            $order->setUser($user);
+
+            // pour chaque livre du panier
+            foreach($user->getBasket()->getBooks() as $book){
+                //ajoute le livre à la commande
+                $order->addBook($book);
+
+                //supprimer le livre du panier
+                $user->getBasket()->removeBook($book);
+            }
+
+
+            //ajouter l'addresse de livrasion à l'utilisateur
+            $user->setDeliveryAddress($payment->address);
+
+            //sauvegarde des données
+            $userrepo->add($user);
+            $orderrepo->add($order);
+            $repository->add($user->getBasket(), true);
+
+            //redirection vers une page de détail
+            return new Response('Commande reçu avec succés');
+        }
+        //affichage de la page de validation du panier
+        return $this->render('front/basket/validate.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
     }
 }
